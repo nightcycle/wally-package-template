ff0016fcf78d278c6d8618790a0c7810ff045e8a
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-07-26 21:08:13 -0600

    Fixed typo in update-packages script

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 wally-update minor
 wally install
-rojo sourcemap default.project.json --output sourcemap.json
+rojo sourcemap dev.project.json --output sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages
