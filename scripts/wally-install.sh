e6e545065be92e1200cea172f49f9dd9ce98e628
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-03-03 13:51:33 -0800

    Update wally-install.sh

--- scripts/wally-install.sh
+++ scripts/wally-install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 ROJO_PROJECT="$1"
+# if ROJO_PROJECT isn't provided, exit
+if [ -z "$ROJO_PROJECT" ]; then
+  echo "Need to supply project file"
+  exit 1
+fi
 wally install
 if [ ! -d "Packages" ]; then
   mkdir Packages
